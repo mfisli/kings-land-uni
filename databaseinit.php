265d26bd39ca7d5e431ae43e4513e3c748f7aedf
Maks Fisli <mfisli2@gmail.com>
2017-08-26 15:15:01 -0700

    added lecture seeds.

--- databaseinit.php
+++ databaseinit.php
@@ -21,6 +21,53 @@ function initTableMajor($conn){
 
 	//selectAllLog($conn, "major");
 }
+function initTableLecture($conn){
+    $q = "CREATE TABLE IF NOT EXISTS lecture (
+		lecture_id     VARCHAR(45) PRIMARY KEY NOT NULL,
+		lecture_title  VARCHAR(45) NOT NULL,
+		building       VARCHAR(45) NOT NULL,
+		room           VARCHAR(45) NOT NULL,
+		start_time     TIME        NOT NULL,
+		end_time 	   TIME        NOT NULL,
+		frequency_code VARCHAR(45) NOT NULL,
+		major_id       VARCHAR(45) NOT NULL,
+		teacher_id     VARCHAR(45) NOT NULL,
+		FOREIGN KEY(major_id)  REFERENCES major(major_id)
+	)";
+    execQuery($conn, $q);
+    $q = "INSERT IGNORE INTO lecture (lecture_id, lecture_title, building, room,  start_time, end_time,frequency_code, major_id, teacher_id) VALUES (
+		'COMP101', 'Intro to Java', 'Cedar', '204', '10:00:00', '10:50:00', 'mwf', 'computer science', 'E11223344'
+	)";
+    execQuery($conn, $q);
+    $q = "INSERT IGNORE INTO lecture (lecture_id, lecture_title, building, room,  start_time, end_time,frequency_code, major_id, teacher_id) VALUES (
+		'COMP110', 'Database I', 'Redwood', '102', '9:00:00', '10:50:00', 'th', 'computer science', 'E37481954'
+	)";
+    execQuery($conn, $q);
+    $q = "INSERT IGNORE INTO lecture (lecture_id, lecture_title, building, room,  start_time, end_time,frequency_code, major_id, teacher_id) VALUES (
+		'COMP155', 'Linear Algebra', 'Oak', '310', '13:00:00', '13:50:00', 'mwf', 'computer science', 'E19283745'
+	)";
+    execQuery($conn, $q);
+    $q = "INSERT IGNORE INTO lecture (lecture_id, lecture_title, building, room,  start_time, end_time,frequency_code, major_id, teacher_id) VALUES (
+		'COMP195', 'Web Development I', 'Cedar', '204', '14:00:00', '14:50:00', 'mwf', 'computer science', 'E11342236'
+	)";
+    execQuery($conn, $q);
+    $q = "INSERT IGNORE INTO lecture (lecture_id, lecture_title, building, room,  start_time, end_time,frequency_code, major_id, teacher_id) VALUES (
+		'ENGL101', 'History of English', 'Oak', '104', '14:00:00', '14:50:00', 'mwf', 'english literature', 'E12345432'
+	)";
+    execQuery($conn, $q);
+    $q = "INSERT IGNORE INTO lecture (lecture_id, lecture_title, building, room,  start_time, end_time,frequency_code, major_id, teacher_id) VALUES (
+		'ENGL104', '16th Century', 'Cedar', '334', '9:00:00', '9:50:00', 'th', 'english literature', 'E11957365'
+	)";
+    execQuery($conn, $q);
+    $q = "INSERT IGNORE INTO lecture (lecture_id, lecture_title, building, room,  start_time, end_time,frequency_code, major_id, teacher_id) VALUES (
+		'ENGL153', 'Rhetoric', 'Redwood', '102', '10:00:00', '11:50:00', 'th', 'english literature', 'E12345432'
+	)";
+    execQuery($conn, $q);
+    $q = "INSERT IGNORE INTO lecture (lecture_id, lecture_title, building, room,  start_time, end_time,frequency_code, major_id, teacher_id) VALUES (
+		'ENGL130', 'Modern American', 'Oak', '106', '13:00:00', '13:50:00', 'mwf', 'english literature', 'E99275439'
+	)";
+    execQuery($conn, $q);
+}
 function initTableStudent($conn){
 	$q = "CREATE TABLE IF NOT EXISTS student (
 		student_id     VARCHAR(45) PRIMARY KEY NOT NULL,
@@ -72,6 +119,7 @@ require_once '../../mysql_conn.php';
 initTableMajor($conn);
 initTableStudent($conn);
 initAccount($conn);
+initTableLecture($conn);
 
 
 /*object(mysqli_result)#3 (5) { 
